c17c2bf6a813ade5b7ae41d9199cb11e97aad99f
Karahan, Faruk (SE-A/35) <faruk.karahan@volkswagen.de>
2023-12-13 19:07:38 +0100

    implemented method to search Customer by Email and an Excception

--- src/main/java/com/amigoscode/customer/CustomerListDataAccessService.java
+++ src/main/java/com/amigoscode/customer/CustomerListDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 @Repository("list")
 public class CustomerListDataAccessService implements CustomerDao {
 
@@ -41,6 +42,17 @@ public class CustomerListDataAccessService implements CustomerDao {
         return customers.stream()
                 .filter(c -> c.getId().equals(id))
                 .findFirst();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return customers.stream()
+                .anyMatch(c -> c.getEmail().equals(email));
     }
 
 
