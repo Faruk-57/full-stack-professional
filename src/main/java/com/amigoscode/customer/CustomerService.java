0518c737ba2c7ffae85dbb61bc3c1c6f53b75d26
Karahan, Faruk (SE-A/35) <faruk.karahan@volkswagen.de>
2024-01-02 10:16:53 +0100

    Solved Put-Request changes on CustomerService

--- src/main/java/com/amigoscode/customer/CustomerService.java
+++ src/main/java/com/amigoscode/customer/CustomerService.java
@@ -56,7 +56,7 @@ public class CustomerService {
 
         if(updateRequest.name() != null && !customer.getName().equals(updateRequest.name())) {
             customer.setName(updateRequest.name());
-            customerDao.insertCustomer(customer);
+            customerDao.updateCustomer(customer);
             changes = true;
         }
         if(updateRequest.email() != null && !customer.getEmail().equals(updateRequest.email())) {
@@ -64,12 +64,12 @@ public class CustomerService {
                 throw new DuplicateResourceException("email already taken");
             }
             customer.setEmail((updateRequest.email()));
-            customerDao.insertCustomer(customer);
+            customerDao.updateCustomer(customer);
             changes = true;
         }
         if(updateRequest.age() != null && !customer.getAge().equals(updateRequest.age())) {
             customer.setAge(updateRequest.age());
-            customerDao.insertCustomer(customer);
+            customerDao.updateCustomer(customer);
             changes = true;
         }
         if (!changes){
